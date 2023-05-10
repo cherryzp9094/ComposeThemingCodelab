507020c5d91c79dc227a7f45032c89ecbc6bd311
cherryzp <cherryzp9094@gmail.com>
2023-05-10 23:29:11 +0900

    feat: 도형 사용

--- app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -17,7 +17,6 @@
 package com.codelab.theming.ui.start
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -46,7 +45,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -211,6 +210,7 @@ fun PostItem(
         icon = {
             Image(
                 painter = painterResource(post.imageThumbId),
+                modifier = Modifier.clip(MaterialTheme.shapes.small),
                 contentDescription = null
             )
         },
